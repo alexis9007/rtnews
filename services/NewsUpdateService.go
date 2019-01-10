db0424f5abcfeb3640d37b12813d63ff93219b90
hoobean1996 <hoobean1996@gmail.com>
2019-01-10 16:14:15 +0800

    update

--- services/NewsUpdateService.go
+++ services/NewsUpdateService.go
@@ -16,7 +16,7 @@ func NewsUpdateService(title string, content string, publishedAt time.Time, auth
 		log.Println("call sqlOpen fatal error ", err)
 		return false, err
 	}
-	stmt := fmt.Sprintf("INSER INTO news VALUES (0,%s, %s,CURRENT_TIMESTAMP(), %s)", title, content, author)
+	stmt := fmt.Sprintf("INSER INTO news VALUES (0,'%s', '%s',CURRENT_TIMESTAMP(), '%s')", title, content, author)
 	log.Println("execute sql statement ", stmt)
 	_, err = db.Exec(stmt)
 	if err != nil {
