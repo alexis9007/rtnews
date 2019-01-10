308fbb009ad3150ab46774cae45e7705f2ec17f7
hoobean1996 <hoobean1996@gmail.com>
2019-01-10 16:09:32 +0800

    updae

--- services/NewsUpdateService.go
+++ services/NewsUpdateService.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -15,11 +16,12 @@ func NewsUpdateService(title string, content string, publishedAt time.Time, auth
 		log.Println("call sqlOpen fatal error ", err)
 		return false, err
 	}
-	stmt := fmt.Sprintf("INSER INTO news VALUES (0,%s, %s, %s, %s)", title, content, publishedAt.String(), author)
+	stmt := fmt.Sprintf("INSER INTO news VALUES (0,%s, %s,CURRENT_TIMESTAMP(), %s)", title, content, author)
 	log.Println("execute sql statement ", stmt)
 	_, err = db.Exec(stmt)
 	if err != nil {
 		log.Printf("EXEC fatal error %v", err)
+		return false, errors.New(fmt.Sprintf("插入数据出现严重错误%v", err))
 	}
 	return true, nil
 
